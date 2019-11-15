0a06a5ba0b266667b2518be7f5be144ba95ae93c
M1g3lAng3l <miguelca959@gmail.com>
2019-11-15 13:33:55 -0600

    Nombres Random
    1 Se termino la pagina
    2 Se implemento un seleccionador de pais y este se valido

--- NumberRandom/src/app/components/principal/principal.component.ts
+++ NumberRandom/src/app/components/principal/principal.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HostListener } from '@angular/core'
 import { DatosPersonasService } from '../../services/datos-personas.service';
+import Swal from 'sweetalert2'
+import { resolve } from 'url';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-principal',
@@ -16,8 +20,30 @@ export class PrincipalComponent implements OnInit {
 
 public datos:any={}
 
+public async MostrarLista(){
+
+  const { value: paises } = await Swal.fire({
+    title: 'Selecciona Un Pais',
+    input: 'select',
+    inputOptions: this._datosPersonas.ListaPaises,
+    inputPlaceholder: 'Selecciona Un Pais',
+
+    inputValidator: (value) => {
+      return new Promise((resolve) => {
+        resolve()
+      })
+    }
+  })
+  this.EleccionPais=paises;
+  if (paises) {
+    Swal.fire(`You selected: ${paises}`)
+  }
+  console.log(paises);
+  
+}
 
 public genero:string=""
+public EleccionPais:string;
 public ActivoA:boolean=true;
 public ActivoF:boolean=false;
 public ActivoM:boolean=false;
@@ -29,7 +55,7 @@ public ActivoM:boolean=false;
       this.ActivoF=false;
       this.ActivoM=false;
       this.genero="";
-      document.getElementById("elInput").focus();
+      // document.getElementById("elInput").focus();
       return 
     }
     if(valor=='F'){
@@ -37,7 +63,7 @@ public ActivoM:boolean=false;
       this.ActivoF=true;
       this.ActivoM=false;
       this.genero="F";
-      document.getElementById("elInput").focus();
+      // document.getElementById("elInput").focus();
       return 
     }
     if(valor=='M'){
@@ -45,7 +71,7 @@ public ActivoM:boolean=false;
       this.ActivoF=false;
       this.ActivoM=true;
       this.genero="M";
-      document.getElementById("elInput").focus();
+      // document.getElementById("elInput").focus();
       return 
     } 
   }
@@ -58,31 +84,84 @@ public ActivoM:boolean=false;
 
   public color="#00"
 
+  public MensajeError(error:HttpErrorResponse){
+    let MsjError=error.error.error
+    // Swal.fire({
+    //   icon: 'error',
+    //   title: 'Oops...',
+    //   text: `${MsjError}`,
+    //   footer: 'Elige Otro Pais'
+    // })
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    })
+    
+    swalWithBootstrapButtons.fire({
+      title: `${MsjError}`,
+      text: "Elige Otro Pais",
+      icon: 'warning',
+      confirmButtonText: 'Eligir Otro Pais',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.value) {
+        this.MostrarLista();
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        this.MostrarLista();
+      }
+    })
+
+
+  }
+   
   public ObtenerDatos(){
 
     console.log(this.genero);
-    this._datosPersonas.DatosPeronas(this.genero).subscribe((info:any)=>{
+    this._datosPersonas.DatosPeronas(this.genero,this.EleccionPais).subscribe(
+      
+      (info:any)=>{
       this.color=this.colores[this.colorAleatorio()]
       this.datos=info;
       console.log(this.datos);
 
       
-    });
+    },
+    (err)=>{
+this.MensajeError(err);
+    }
+    
+    
+    
+    
+    );
   }
   
   constructor(private _datosPersonas:DatosPersonasService) {
 
-this._datosPersonas.DatosPeronas(this.genero).subscribe((info:any)=>{
+// this._datosPersonas.DatosPeronas(this.genero,this.EleccionPais).subscribe((info:any)=>{
   
-      this.datos=info;
-      console.log(this.datos);
-      document.getElementById("espacio").focus();
-      // this.ocultar=true;
-    });
+//       this.datos=info;
+//       console.log(this.datos);
+//       document.getElementById("espacio").focus();
+//       this.ocultar=true;
+//     });
  
   }
 
   ngOnInit() {
   }
 
+  @HostListener("document:keypress",["$event"])
+    KeyBoarEvent(event){
+      if(event.code!="Space")return
+      this.ocultar=true;
+      this.ObtenerDatos();
+    }
 }
+
