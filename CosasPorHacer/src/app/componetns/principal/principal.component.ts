12c07d5c9fa15ccc4385df7fbcbc8a69c4e6a5f6
M1g3lAng3l <miguelca959@gmail.com>
2019-11-26 10:45:46 -0600

    Thing For Do (Terminado)
    Se agregaron sweetAlert
    Se hicieron los metodos de put, get y delete

--- CosasPorHacer/src/app/componetns/principal/principal.component.ts
+++ CosasPorHacer/src/app/componetns/principal/principal.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ForDoServicesService } from '../../services/for-do-services.service';
 import { contentThing, Thing } from '../../interfaces/thing.interface';
-
+import Swal from 'sweetalert2'
 
 
 @Component({
@@ -15,12 +15,30 @@ export class PrincipalComponent implements OnInit {
   public Formulario:FormGroup;
 
   public obtenerThing(){
+
+    const Toast = Swal.mixin({
+      toast: true,
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+    })
+  
 console.log(this.Formulario.value);
 
 this._ForDoThings.postThing(this.Formulario.value).subscribe(
   (data)=>{
+      
+    Toast.fire({
+      icon: 'success',
+      title: 'Guardado Correctamente'
+    })
+
+    this.Formulario.reset();
+
   console.log(data);
 })
+
+
 }
 
   private crearFormulario(){
@@ -39,6 +57,40 @@ public things:contentThing[]
     })
   }
 
+  public cambiarStatus(thing:contentThing){
+    thing.complete=!thing.complete;
+    console.log(thing);  
+    this._ForDoThings.putThing(thing).subscribe(
+    (data)=>{},
+    (error)=>{console.log(error);
+    }
+    );
+  }
+
+  public eliminarTarea(thingId){
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.value) {
+        this._ForDoThings.deletThing(thingId).subscribe(()=>{
+          Swal.fire(
+            'Deleted!',
+            'Your file has been deleted.',
+            'success'
+          )
+        });
+
+      }
+    })
+    console.log(thingId);
+    
+  }
 
   constructor(private _ForDoThings:ForDoServicesService) {
 
--- CosasPorHacer/src/app/services/for-do-services.service.ts
+++ CosasPorHacer/src/app/services/for-do-services.service.ts
@@ -1,27 +1,42 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
+import { contentThing } from '../interfaces/thing.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ForDoServicesService {
 
-  private urlGlobal='http://10.2.47.15:5000';
+  private urlGlobal='http://10.2.47.48:5000';
 
   public postThing(thing){
 
-    let url=`${this.urlGlobal}/v1/things`
+    let url=`${this.urlGlobal}/v1/things`;
     return this.http.post(url, thing)
 
   }
 
 
   public getThing():Observable<object>{
-
-    let url=`${this.urlGlobal}/v1/things`
+    let url=`${this.urlGlobal}/v1/things`;
     return this.http.get(url);
   }
+
+public putThing(thing:contentThing){
+
+  let url=`${this.urlGlobal}/v1/things/${thing._id}`
+  return this.http.put(url,thing)
+
+}
+
+public deletThing(thingId){
+
+  
+  let url=`${this.urlGlobal}/v1/things/${thingId}`
+  return this.http.delete(url);
+
+}
 
   constructor(private http:HttpClient) { }
 }
